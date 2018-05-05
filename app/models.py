9b06b5e0d2d42e7a9066550047df6ea073d23037
momojohnson <momo.johnson1987@gmail.com>
2018-05-05 08:48:07 -0500

    Updated models file by changing Users model from users to websiteadmins

--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
 
-class User(UserMixin, db.Model):
-    """ Create a user table """
-    __tablename__ = 'users'
+class WebsiteAdmin(UserMixin, db.Model):
+    """ Create a Site admin table """
+    __tablename__ = 'website_admins'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(60), unique=True)
     email = db.Column(db.String(120), unique=True)
