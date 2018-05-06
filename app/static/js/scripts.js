6fa32f9fd430be3ebe529f4865f7af69db25461d
momojohnson <momo.johnson1987@gmail.com>
2018-05-06 15:03:05 -0500

    Added media query to website for smaller devices, medium and desktop

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,6 +1,6 @@
 $(function(){
   $(".typed").typed({
-  		strings: ["Java web and desktop applications.", "Python Web Applications.", " C# desktop applications.", "JavaScript Web Applications."],
+  		strings: ["Java desktop applications.", "Python Web Applications.", " C# desktop applications.", "JavaScript Web Applications."],
   		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
   		stringsElement: null,
   		// typing speed
